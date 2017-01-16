b306c09130001c6d058456c328e2610db97eb2dc
oszust <oszust@memleak.pl>
2017-01-16 01:23:42 +0100

    More generic implementation added, problems with system_limit, not much more performance

--- src/generation.erl
+++ src/generation.erl
@@ -14,44 +14,58 @@
 %% API
 -compile(export_all).
 %%-export([]).
-getTreeValue(Board, Move, 0, Color, _, _) ->
+getTreeValue(Move, Board, 0, Color, _, _) ->
   {evaluateBoard(Color, makeMove(Board, Move)), Move};
 
-getTreeValue(Board, Move, Levels, Color, MoveColor, min) ->
+getTreeValue(Move, Board, Levels, Color, MoveColor, min) ->
   NewBoard = makeMove(Board, Move),
   ReverseColor = reverseColor(MoveColor),
   case getPermittedMoves(NewBoard, ReverseColor) of
     [] -> {evaluateBoard(Color, NewBoard), Move};
-    List -> {Val,_} = lists:min([getTreeValue(NewBoard, X, Levels-1, Color, ReverseColor, max) || X <- List]),
-            {Val, Move}
+    List ->
+      S = self(),
+      PidMoveMap = lists:map(fun(Element) ->
+        {spawn_link(fun() ->
+          execute(S, getTreeValue, [Element, NewBoard, Levels-1, Color, ReverseColor, max]) end), Element} end, List),
+      {Val, _} = lists:min(getMoveScores(PidMoveMap, S, getTreeValue, [NewBoard, Levels-1, Color, ReverseColor, max])),
+      {Val, Move}
   end;
 
-getTreeValue(Board, Move, Levels, Color, MoveColor, max) ->
+getTreeValue(Move, Board, Levels, Color, MoveColor, max) ->
   NewBoard = makeMove(Board, Move),
   ReverseColor = reverseColor(MoveColor),
   case getPermittedMoves(NewBoard, ReverseColor) of
     []  -> {evaluateBoard(Color, NewBoard), Move};
-    List -> {Val,_} = lists:max([getTreeValue(NewBoard, X, Levels-1, Color, ReverseColor, min) || X <- List]),
-            {Val, Move}
+    List ->
+      S = self(),
+      PidMoveMap = lists:map(fun(Element) ->
+        {spawn_link(fun() ->
+          execute(S, getTreeValue, [Element, NewBoard, Levels-1, Color, ReverseColor, min]) end), Element} end, List),
+      {Val, _} = lists:max(getMoveScores(PidMoveMap, S, getTreeValue, [NewBoard, Levels-1, Color, ReverseColor, min])),
+      {Val, Move}
   end.
 
-calculateGameValue(Pid, Board, Pos, Levels, Color) ->
-  Pid ! {self(), getTreeValue(Board,Pos,Levels, Color, Color, min)}.
+execute(Pid, Function, Args) ->
+  Pid ! {self(), erlang:apply(?MODULE, Function, Args)}.
 
 getTheBestMove(Board, Color, Levels) ->
   PermittedMoves = getPermittedMoves(Board, Color),
   S = self(),
   PidMoveMap = lists:map(fun(Element) ->
-    {spawn_link(fun() -> calculateGameValue(S,Board,Element,Levels,Color) end), Element} end, PermittedMoves),
-  {_,Move} = lists:max(getMoveScores(PidMoveMap)),
+    {spawn_link(fun() -> execute(S,getTreeValue,[Element,Board,Levels,Color,Color, min]) end), Element} end, PermittedMoves),
+  {_,Move} = lists:max(getMoveScores(PidMoveMap,S, getTreeValue, [Board, Levels, Color, Color, min])),
   Move.
 
 
 
-getMoveScores([]) ->
+getMoveScores([],_,_,_) ->
   [];
-getMoveScores(PidMoveMap) ->
+getMoveScores(PidMoveMap, ParentPid, RestartFunc, Args) ->
   receive
     {Pid, {Value,Move}} ->
-      [{Value, Move}| getMoveScores(lists:keydelete(Pid,1,PidMoveMap))]
+      [{Value, Move}| getMoveScores(lists:keydelete(Pid,1,PidMoveMap), ParentPid, RestartFunc, Args)];
+    {'EXIT', ExitPid, _} ->
+      {MoveValue,_,NewList} = lists:keytake(ExitPid, 1, PidMoveMap),
+      getMoveScores([spawn_link(fun() ->
+        execute(ParentPid, RestartFunc, [MoveValue|Args]) end) | NewList], ParentPid, RestartFunc, Args)
   end.
